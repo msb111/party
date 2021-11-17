78202b0693434969a5a5acc9a8e4ce5c2fe1d9a6
Fred <no@email.com>
2021-11-17 05:25:20 +0200

    Updates

--- packages/contracts/src/addresses.js
+++
@@ -1,8 +0,0 @@
-// This address points to a dummy ERC20 contract deployed on Ethereum Mainnet,
-// Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
-const addresses = {
-  ptyErc20: "0x4AC1242106601F70BfDc4731D4EEAF7f7501B5Cd",
-  faucet: "0xfd1D23F823dA379a8AA7ADFAf02ed0ce624E1642",
-};
-
-export default addresses;
